908f3fc3dfaf1e7a44b74a2bd8f6aeda0dbbce42
Wade King <wkingnumber2@gmail.com>
2019-04-02 16:15:17 -0700

    RSA functionality completed

--- client.py
+++ client.py
@@ -7,11 +7,10 @@ import sympy
 import random
 
 encMode = None
+params = None
 
 
-#------------ AFFINE CIPHER
-a = None
-b = None
+
 
 
 #------------ mod inv and gcd function courtesy of wikibooks
@@ -30,20 +29,26 @@ def modinv(a, m):
     else:
         return x % m 
 
+def stringToTup(msg):
+    msg = msg.strip('#rsa ()\n')
+    msg = msg.split(',')
+    return tuple(msg)
 
 
-def isCommand(mes):
-    return re.search("#set", mes)
+
+
+def isParams(mes):
+    return re.search("#(affine)\s\(.*\)",mes)
 
 def isEnc(mes):
     return re.search("#enc\(.*\)", mes)
 
-def getMode(mes):
-    return re.search("(?<=#set\s)\w+", mes).group(0)
+def getParamsMode(mes):
+    return re.search("#(\w+)",mes).group(1)
 
-def getTag(mes, tag):
-    pat = "(?<=#set\s).*-a\s(\w+)"
-    return re.search("(?<=#set\s).*-"+tag+"\s(\w+)", mes).group(1)
+def getParams(mes):
+    return re.search("#(\w+)\s(\(.*\))",mes).group(2)
+
 
 def getEnc(mes):
     return re.search("#enc\((.*)\)", mes).group(1)
@@ -63,12 +68,12 @@ def RSAinit(rang_low, rang_mid, rang_high):
     while(egcd(b, phi)[0] != 1):
         b = random.randint(2, phi)
     
-    a = modinv(b, n)
+    a = modinv(b, phi)
 
     return ((n,b),(p,q,a))
 
 def RSAdec(rsaMsg, key):
-    return pow(int(rsaMes), kpr[1][2], kpr[0][0])
+    return pow(int(rsaMsg), int(key[1][2]), int(key[0][0]))
 
 
 
@@ -81,12 +86,13 @@ Port = int(sys.argv[2])
 server.connect((IP_address, Port))
 
 #initialise RSA key
-key = RSAinit(100,200,300)
+key = RSAinit(1000,2000,3000)
 kpu = key[0]
 kpr = key[1]
-print(key)
 
+#send the public key to the server
 server.send(("#rsa "+str(kpu)).encode())
+
   
 while True: 
   
@@ -115,16 +121,24 @@ while True:
 
                 plaintext = ""
                 if encMode == "affine":
+                    a, b, n = params
                     #calculate the inverse of a
-                    ainv = modinv(a, 126)
+                    ainv = modinv(a, n)
 
                     #decrypt the message
                     for c in message:
-                        chval = (ainv*ord(c) - ainv*b)%126
+                        chval = (ainv*ord(c) - ainv*b)%n
                         if chval < 0:
-                            chval += 126
+                            chval += n
                         plaintext += (chr(chval))
-                    print(addr+">"+plaintext)
+                    print(addr+"> "+plaintext)
+            elif(isParams(message)):
+                print("Affine Cypher updated")
+                encMode = getParamsMode(message)
+                encParams = getParams(message)
+                encParams = stringToTup(encParams)
+
+                params = [RSAdec(int(x), key) for x in encParams]
             else:
                 print(message)
         else: 
@@ -132,38 +146,50 @@ while True:
             
 
             message = message.strip('\n')
-
-            #------- set encryption params -------------
-
-            #determine the encryption mode before sending
-            #don't send the set encryption parameters command to the
-            #server for security reasons
-            if(isCommand(message)):
-                encMode = getMode(message)
-                if(encMode=="affine"):
-                    a = int(getTag(message, 'a'))
-                    b = int(getTag(message, 'b'))
-
-
-            #-------- apply encryption ----------------
-                
+            
+            if(encMode and isEnc(message)):
+                message = getEnc(message)
+                cypher = "#enc("
+                #apply affine cipher
+                if(encMode == "affine"):
+                    a, b, n = params
+                    for c in message:
+                        cypher += (chr((a*ord(c) + b)%n))
+                    cypher+=")"
+                    server.send(cypher.encode())
             else:
-                #if encryption mode is set and message is not
-                #plaintext
-                if(encMode and isEnc(message)):
-                    message = getEnc(message)
-                    cypher = "#enc("
-                    #apply affine cipher
-                    if(encMode == "affine"):
-                        for c in message:
-                            cypher += (chr((a*ord(c) + b)%126))
-                        cypher+=")"
-                        server.send(cypher.encode())
+                #server.send(message.encode())
+                if("#quit" == message):
+                    server.send(message.encode())
+                    exit()
+                elif("#help" == message):
+                    message_to_send ="""+----------+
+|CryptoChat|
++----------+
+
+CryptoChat is a cryptography based chat room that allows you to choose
+how you encrypt your messages
+
+CryptoChat has RSA encryption built in for key sharing between clients.\n
+when you choose a symmetric key cryptosystem the server chooses a key for\n
+all the clients and sends it to each of the clients via RSA encryption\n
+
+COMMANDS:
+
+#help | displays this help message
+
+#affine | sets encryption method to affine cypher
+
+#enc(text) | encrypts the text given in the parenthesis
+according to the encryption method
+
+#quit | leave the chat room"""
+                    print(message_to_send)
+                
                 else:
                     server.send(message.encode())
-                    if("#quit" == message):
-                        exit()
-                    
+                
+                
                 
 
             
--- server.py
+++ server.py
@@ -1,19 +1,34 @@
 # Python program to implement server side of chat room. 
 import socket 
 import select 
-import sys 
+import sys
+import _thread
 from _thread import *
 import random
 import re
 
+mutex = _thread.allocate_lock()
+
 def RSAenc(rsaMsg, kpu):
-    return pow(int(rsaMsg), kpu[1], kpu[0])
+    return pow(int(rsaMsg), int(kpu[1]), int(kpu[0]))
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
 
 def isRSA(msg):
-    return re.search("^#rsa")
+    return re.search("#rsa", msg)
 
 def getRSA(msg):
-    return re.search("(?<=rsa\s).*").group(0)
+    return re.search("(?<=#rsa\s).*", msg).group(0)
+
+def stringToTup(msg):
+    msg = msg.strip('#rsa ()\n')
+    msg = msg.split(',')
+    return tuple(msg)
   
 """The first argument AF_INET is the address domain of the 
 socket. This is used when we have an Internet Domain with 
@@ -47,29 +62,73 @@ increased as per convenience.
 """
 server.listen(2) 
   
-list_of_clients = [] 
+list_of_clients = []
+
+#dictionary of public keys
+client_pub_keys = {}
   
+
 def clientthread(conn, addr):
   
     # sends a message to the client whose user object is conn 
-    conn.send("Welcome to this chatroom! type #quit to leave".encode()) 
+    conn.send("Welcome to CryptoChat! type #quit to leave or #help for help".encode()) 
   
     while True: 
             try: 
                 message = conn.recv(2048) 
                 if message: 
-  
+                    message = message.decode().strip('\n')
                     """prints the message and address of the 
                     user who just sent the message on the server 
                     terminal"""
-                    print("<" + addr[0] + "> " + message.decode())
-                    if("#quit" == message.decode().strip(' \n')):
+                    print("<" + addr[0] + "> " + message)
+                    
+                    if("#quit" == message):
                         remove(conn)
                         message_to_send = "<" + addr[0] + "> has left the chat"
                         broadcast(message_to_send, conn)
+                        #server ignores #help messages
+                    if("#help" == message):
+                        pass
+                    #get the public key of the client
+                    elif(isRSA(message)):
+                        kpu = getRSA(message)
+                        kpu = stringToTup(message)
+                        client_pub_keys.update({conn : kpu})
+                    #client has requested keys for affine cypher
+                    elif("#affine" == message):
+                        print("got here")
+                        #lock this critical section
+                        mutex.acquire()
+                        print("lock aquired")
+                        n = 126
+                        a = random.randint(1, 126)
+                        #make sure a is invertable
+                        while(egcd(a,n)[0]!=1):
+                            a = random.randint(1,126)
+
+                        b = random.randint(1, 126)
+
+                        print("affine cypher parameters: "+str((a,b,n)))
+
+                        for conn in list_of_clients:
+                            kpu = client_pub_keys[conn]
+
+                            package = (RSAenc(a,kpu), RSAenc(b,kpu), RSAenc(n, kpu))
+                            print("encrypted affine cypher params: "+str(package))
+
+                            message_to_send = "#affine "+str(package)
+
+                            conn.send(message_to_send.encode())
+                        #unlock mutex
+                        mutex.release()
+                    elif("#list" == message):
+                        conn.send(str(list_of_clients).encode())
+                        
+                        
                     else:
                         # Calls broadcast function to send message to all 
-                        message_to_send = "<" + addr[0] + "> " + message.decode() 
+                        message_to_send = "<" + addr[0] + "> " + message
                         broadcast(message_to_send, conn) 
   
                 else: 
@@ -100,6 +159,7 @@ the program"""
 def remove(connection): 
     if connection in list_of_clients: 
         list_of_clients.remove(connection) 
+        client_pub_keys.pop(connection, None)
   
 while True: 
   
@@ -108,10 +168,9 @@ while True:
     which contains the IP address of the client that just  
     connected"""
     conn, addr = server.accept() 
-  
     """Maintains a list of clients for ease of broadcasting 
     a message to all available people in the chatroom"""
-    list_of_clients.append(conn) 
+    list_of_clients.append(conn)
   
     # prints the address of the user that just connected 
     print(addr[0] + " connected")
